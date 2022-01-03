fe783b9794a0a90a360a333e4ac478f9fc392dbf
suporte <suporte@suportes-MacBook-Pro.local>
2022-01-03 15:29:42 -0300

    Next level

---
+++ next-site/components/header/index.tsx
@@ -0,0 +1,11 @@
+import { container, title } from "./styles"
+
+const Header = () => (
+<header css={container}>
+        <h1 css={title}>
+          Yay! My next website!
+        </h1>
+        </header>
+)
+
+export default Header
---
+++ next-site/components/header/styles.ts
@@ -0,0 +1,17 @@
+
+import { css } from "@emotion/react";
+
+const container = css`
+  min-height: 100vh;
+  padding: 4rem 0;
+  flex: 1;
+  display: grid;
+  `
+
+
+const title = css`
+  color: #0070f3;
+  text-decoration: none;
+`
+
+export { container, title}
